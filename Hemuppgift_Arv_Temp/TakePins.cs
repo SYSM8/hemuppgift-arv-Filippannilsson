56061460e714277a7a203825fc3961cdb866be8b
Filippannilsson <filippanilsson95@gmail.com>
2024-10-10 12:24:47 +0200

    Svarat på de första frågorna

--- Hemuppgift_Arv_Temp/TakePins.cs
+++ Hemuppgift_Arv_Temp/TakePins.cs
@@ -2,7 +2,13 @@
 {
     internal class TakePins
     {
-        //Test
+        /* Frågor att svara på:
+        1. Klassen Player är superklass till klasserna Humanplayer och ComputerPlayer.
+        2. Vilka av följande tilldelningssatser är korrekta?
+            Player p = new HumanPlayer("Alicent");
+            HumanPlayer p = new HumanPlayer("Rhaenyra");
+        */
+
         //Här är main klassen där koden ska testas, lägg in i mappen
         static void Main(string[] args)
         {
